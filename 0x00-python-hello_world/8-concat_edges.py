fc3a0859aa7c6facdd2d62bafe72bdc6198cddc7
fredmugambi <fredmugambi7@gmail.com>
2023-08-29 22:03:03 -0700

    0x00-python-hello_world

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str=str[:6], str[43:61], str[71:83], str[5:15], str[106:116], str[25:43], str[50:57], str[18:25], str[88:97],str[118:])
+str=str[:6]+str[43:61]+ str[71:83]+ str[5:15]+ str[106:116]+str[25:43]+ str[50:57]+ str[18:25]+ str[88:97]+str[118:])
 print(str)
