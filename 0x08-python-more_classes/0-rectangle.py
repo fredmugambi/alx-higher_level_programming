5167bc4b89461dd006f3ae101fb4abbc3d38f4ed
fredmugambi <fredmugambi7@gmail.com>
2023-09-05 22:53:26 -0700

    0x08-python-more_classes

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python3
+"""AN empty rectangle"""
+
 class Rectangle:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+""" Reps an empty Rectangle  """
+        pass
